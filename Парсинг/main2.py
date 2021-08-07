6dba579617df7f100115e5ade1c742cd763fb23e
Ratmir <ratmir.yuldashev28@mail.ru>
2021-08-08 01:29:42 +0600

    parser

---
+++ Парсинг/main2.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+s = requests.Session()
+
+# get CSRF
+
+auth_html = s.get("https://smartprogress.do/")
+auth_bs = BS(auth_html.content, "html.parser")
+csrf = auth_bs.select("input[name=YII_CSRF_TOKEN]")[0]['value']
+
+print(csrf)
+
+#do login
+
+payload = {
+	"YII_CSRF_TOKEN": csrf,
+	"returnUrl": "/",
+	"UserLoginForm[email]": "ratmir.yuldashev28@mail.ru",
+	"UserLoginForm[password]": "28JUne2004",
+	"UserLoginForm[rememberMe]": 1
+}
+
+answ = s.post("https://smartprogress.do/user/login", data = payload)
+answ_bs = BS(answ.content, "html.parser")
+
+print("Имя: {}\nУровень: {}\nОпыт: {}".format(
+	answ_bs.select(".user-menu__name")[0].text.strip(),
+	answ_bs.select(".user-menu__info-text--lvl"+"1")[0].text.strip(),
+	answ_bs.select(".user-menu__info-text--exp")[0].text.strip()
+) )
